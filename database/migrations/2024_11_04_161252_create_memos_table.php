02369f7ca68824dcd08d0cfd9b4c2927a8aef857
wadashuri <wada20010108@icloud.com>
2024-11-04 16:41:16 +0900

    DB周り作成

---
+++ database/migrations/2024_11_04_161252_create_memos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title')->nullable()->comment('メモのタイトル');
+            $table->json('content')->nullable()->comment('メモの内容');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memos');
+    }
+};
