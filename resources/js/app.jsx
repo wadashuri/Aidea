6e4b32e52ff2efbe78c413834d4ec10d9285f1ae
wadashuri <wada20010108@icloud.com>
2024-11-04 17:48:06 +0900

    間違えて消してしまったコードを復元

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,14 @@
+import './bootstrap';
+import Alpine from 'alpinejs';
 import React from "react";
 import { createRoot } from "react-dom/client";
 import ExampleComponent from "./components/ExampleComponent";
 
+// Alpine.jsの初期化
+window.Alpine = Alpine;
+Alpine.start();
+
+// Reactコンポーネントのレンダリング
 const container = document.getElementById("example");
 if (container) {
     const root = createRoot(container);
