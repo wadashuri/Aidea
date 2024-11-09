d5be435a6dde2d305535af2a391b118217bdab4c
Wadashuri <wada20010108@icloud.com>
2024-11-09 18:37:34 +0900

    サイドバー作成

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,7 +16,7 @@
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="h-full font-sans antialiased">
         @inertia
     </body>
 </html>
