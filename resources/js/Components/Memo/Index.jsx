f360dc117f620f7f001b42bb7a344dd85ad217b5
Wadashuri <wada20010108@icloud.com>
2024-11-10 14:16:27 +0900

    取得関数をカスタムフックとして記載するように修正

--- resources/js/Components/Memo/Index.jsx
+++ resources/js/Components/Memo/Index.jsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import FetchAll from "../../Services/Api/FetchAll";
+import React, { useMemo } from 'react';
+import UseFetchAll from '../../Hooks/UseFetchAll';
 import { Link } from '@inertiajs/react';
 
 export default function IndexMemo() {
-    const [data, setData] = useState([]);
+    const { data, loading, error } = UseFetchAll(route('memo.index'));
 
-    useEffect(() => {
-        FetchAll(route('memo.index')).then(setData);
-    }, []);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
 
     return (
         <ul>
             {data.map((memo) => (
-                <Link key={memo.id} href={route('memo.show', {memoId : memo.id})}>
+                <Link key={memo.id} href={route('memo.show', { memoId: memo.id })}>
                     <li className="px-4 py-2 text-white hover:text-white hover:bg-gray-700">
                         {memo.title || "新規メモ"}
                     </li>
